0e175ab85ff93f0a548a17892ec4b674fce6620c
Kristian <7685505@student.swin.edu.au>
2015-09-21 22:43:47 +1000

    fix-code3
    All fixes applied until "NOTE:..." section. Game compiles and runs as
    expected.

--- src/GameState.cs
+++ src/GameState.cs
@@ -4,6 +4,14 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;
+using static GameController;
+using static UtilityFunctions;
+using static GameResources;
+using static DeploymentController;
+using static DiscoveryController;
+using static EndingGameController;
+using static MenuController;
+using static HighScoreController;
 
 /// <summary>
 /// The GameStates represent the state of the Battleships game play.
