12fdc781c3204e31e0d5c3db2409c3fcd672a075
brenobrien <7666446@student.swin.edu.au>
2015-09-29 16:13:37 +1000

    change code from hard to medium

--- src/GameController.cs
+++ src/GameController.cs
@@ -89,7 +89,7 @@ public static class GameController
 				_ai = new AIHardPlayer(_theGame);
 				break;
 			default:
-				_ai = new AIHardPlayer(_theGame);
+				_ai = new AIMediumPlayer(_theGame);
 				break;
 		}
 
